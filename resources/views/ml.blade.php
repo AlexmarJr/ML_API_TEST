eb0a2e0e8cc073394be69f01b26bada499149fc5
Alexmar Ramos Noronha Junior <alexmar.yiy@gmail.com>
2022-08-01 21:02:17 -0300

    post

--- resources/views/ml.blade.php
+++ resources/views/ml.blade.php
@@ -9,6 +9,7 @@
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     </head>
     <style>
@@ -118,6 +119,11 @@
     </body>
 
     <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
         
         function getMLData(){
             $('#dinoModal').modal('show');
@@ -126,7 +132,7 @@
             let q = $('#search').val()
             $.ajax({
                 url:"/loadData/",
-                type: "get",
+                type: "post",
                 dataType: 'json',
                     success: function (response) {
                         $("#loading").css("display", "none");   
